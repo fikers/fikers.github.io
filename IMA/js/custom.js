ca6ce868e1a87c392993a9e3ce8e31c59f9360aa
Faique <faiquemoqeet@gmail.com>
2017-04-20 22:09:33 -0500

    Changes

---
+++ IMA/js/custom.js
@@ -0,0 +1,6 @@
+// window.onload = function(){
+//   var services = document.querySelectorAll('.servicebutton');
+//   services.forEach(addEventListener('click', function(){
+//     console.log('clicked');
+//   }))
+// }
